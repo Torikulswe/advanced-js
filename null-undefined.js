040286d67f988dc9248ca8bd4e9af99952abb924
Torikul Islam Tanim <torikulswe@gmail.com>
2021-02-04 11:07:52 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,10 @@
+//let pakhi;
+// function add(num1,num2){
+//     console.log(num1 + num2);
+// }
+
+// const man = {name:"smart", phone:12345};
+// console.log(man.gf);
+
+// let ages = [2,4,5];
+// console.log(ages[22]);
